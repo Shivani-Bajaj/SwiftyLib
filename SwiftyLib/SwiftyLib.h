b7a1a55aaed55d47b9838cb60411c8ded7f6eced
shivani Bajaj <shivani.bajaj@isteams-Mac-mini-139.local>
2019-09-17 12:19:53 +0530

    Initial project setup

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by shivani Bajaj on 9/17/19.
+//  Copyright © 2019 shivani Bajaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
